3d1037dd92780a7855312347dc3f67add2ed7fb1
joemartinson <martinsonjoe2@gmail.com>
2020-11-22 19:56:58 -0800

    major improvements

--- mkopendir.sh
+++ mkopendir.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-cp template.html output.html
-du -al "$1" | awk '{print $2}' | xargs -I % sh -c 'echo "<p><a href=\"%\">%</a></p>" >> output.html'
-echo "$(cat tempend.html)" >> output.html
+echo "<html>\n   <body>\n   <h3><hr><pre>" > output.html
+du -al "$1" | awk '{print $2}' | xargs -I % sh -c 'echo "        <a href=\"%\">%</a>" >> output.html'
+echo "      </h3></hr></pre>\n   </body>\n</html>" >> output.html
